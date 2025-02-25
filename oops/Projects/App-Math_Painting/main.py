a515ca9e807168814844f52df829fd09e276f64d
Ashish <a9ever@gmail.com>
2025-02-25 21:25:53 +0530

    added my first stand alone small project

--- oops/Projects/App-Math_Painting/main.py
+++ oops/Projects/App-Math_Painting/main.py
@@ -1,16 +1,45 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from PIL import Image
+from numpy import dtype
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+class Canvas:
+    def __init__(self, height, width, color):
+        self.color = color
+        self.height = height
+        self.width = width
+
+        #create a 3d numpy array of zeros
+        self.data = np.zeros((self.height, self.width,3), dtype=np.uint8)
+
+    def make(self):
+        pass
+
+class Rectangle:
+    def __init__(self, x,y,a,b,color):
+        self.x = x
+        self.y = y
+        self.a = a
+        self.b = b
+        self.color = color
+
+    def draw(self, canvas):
+        pass
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+class Square:
+    def __init__(self, x, y, a, color):
+        self.x = x
+        self.y = y
+        self.a = a
+        self.color = color
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    def draw(self, canvas):
+        pass
+
+canvas = Canvas(height=20, width=30, color=(255,255,255))
+r1 = Rectangle(x=1,y=6, height=7,width=10,color=(100,200,125))
+r1.draw(canvas)
+s1=Square(x=1,y=3,side=3,color=(0,100,222))
+s1.draw(canvas)
+canvas.make('canvas.png')
