59872e0509b4ad51f9585c4ce95af173ea0b8cb3
Ashish <a9ever@gmail.com>
2025-02-25 21:35:54 +0530

    initial push

---
+++ my_projects/rename_files_and_add_dates/main.py
@@ -0,0 +1,18 @@
+from datetime import  datetime
+import os
+
+#date = datetime.now().strftime("%a-%d-%m-%Y")
+date = datetime.now().strftime("%Y-%m-%d")
+
+my_dir = os.listdir("files_dir")
+for i in my_dir:
+    filenames = "files_dir/"+i
+    print(filenames)
+
+    with open (filenames, 'r') as my_file:
+        content = my_file.read()
+        content = len(content.split())
+
+    new_name = f"{filenames[:-4]}-{date}_words_{content}.txt"
+    os.rename(filenames, new_name)
+    print(f"Old name is {filenames}, and new name is {new_name}")
